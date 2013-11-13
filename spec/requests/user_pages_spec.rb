494d3427c9b203a72e45754087da79299d92bd89
ivawzh <raywzh@hotmail.com>
2013-11-13 16:04:01 +1100

    improved after invalid/valid signup test, and used content_tag for flash message.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,9 +4,9 @@ describe "UserPages" do
   subject {  page  }
 
 
-  describe "profile page" do
-    let(:user) {  FactoryGirl.create(:user)  }
+  describe "profile page - model test" do
     before {  visit user_path(user)  }
+    let(:user) {  FactoryGirl.create(:user)  }
     it {  should have_content(user.name)  }
     it {  should have_title(user.name)  }
   end
@@ -19,7 +19,7 @@ describe "UserPages" do
   end
 
 
-  describe "signup" do
+  describe "signup - " do
 
     before { visit signup_path }
 
@@ -29,7 +29,15 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+
+    describe "after invalid submission" do
+        before {click_button submit}
+        it { should have_title('Sign up')}
+        it { should have_content('error')}
+      end
     end
+
 
     describe "with valid information" do
       before do
@@ -39,8 +47,18 @@ describe "UserPages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+
+      describe "after saving the user" do
+        before { click_button submit }
+        it { should have_content("Welcome") }
+        let(:user) {User.find_by(email:"user@example.com")}
+        it { should have_title(user.name)}
+        it { should have_selector('div.alert.alert-success', text:"Welcome")}
       end
     end
   end
