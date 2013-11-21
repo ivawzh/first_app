dfb2c7c0db686865b4475a8946f4da863891760f
ivawzh <raywzh@hotmail.com>
2013-11-21 20:22:20 +1100

    changed sign_in? method back to old one

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,13 @@
 module SessionsHelper
   def sign_in(user)
-    remember_token= User.new_remember_token  #generate a new token
-    cookies.permanent[:remember_token]=remember_token #save the unencrypted token to browser cookies
-    user.update_attribute(:remember_token, User.encrypt(remember_token)) #save the encrypted token to database
-    self.current_user= user #tell application the user is signed in and who he is
+    #generate a new token
+    remember_token= User.new_remember_token
+    #save the unencrypted token to browser cookies
+    cookies.permanent[:remember_token]=remember_token
+    #save the encrypted token to database
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    #tell application the user is signed in and who he is
+    self.current_user= user
   end
 
   def current_user=(user)
@@ -15,18 +19,18 @@ module SessionsHelper
     @current_user ||=User.find_by(remember_token:encrypted_token)
   end
 
-  #def sign_in?
-  #  !current_user.nil?
-  #end
-
-
   def sign_in?
-    if @current_user.nil?
-      !current_user.nil?
-    else
-      false
-    end
+    !current_user.nil?
   end
+
+
+  #def sign_in?
+  #  if @current_user.nil?
+  #    !current_user.nil?
+  #  else
+  #    false
+  #  end
+  #end
 
 
 
